a5dcc28cf99244bf89ec9f9bcee21e7dbd255962
unknown <elsa.ferreiracalvo@iesmiguelherrero.com>
2024-09-20 20:13:27 +0200

    cambios

---
+++ DWES_Hoja01/hoja02_PHP_01/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+        <meta charset="UFT-8">
+        <title>Desarrollo web</title>
+</head>
+
+<body>
+
+    <?php
+        echo "<b>Elsa Ferreira, Torrelavega </b>"
+    ?>;
+</body>
+</html>
+
