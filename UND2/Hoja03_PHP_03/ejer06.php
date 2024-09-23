76f55cce32ff1b95b1ff773f5d4cd56417cc2a5f
Elsa <115722474+s0y3lsa@users.noreply.github.com>
2024-09-23 20:00:29 +0200

    Update ejer06.php

---
+++ UND2/Hoja03_PHP_03/ejer06.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title> Ejercicio3 </title>
+    </head>
+
+    <body>
+        <?php
+            $a = 8;
+            $b = 3;
+            $c = 5;
+            
+            print"Operaciones: <br/>";
+            print"a == b: " . ($a == $b ? "Verdadero" : "Falso") . "<br/>";
+            print"a != b: " . ($a != $b ? "Verdadero" : "Falso") . "<br/>";
+            print"a < b: " . ($a < $b ? "Verdadero" : "Falso") . "<br/>";
+            print"a > b: " . ($a > $b ? "Verdadero" : "Falso") . "<br/>";
+            print"a >= c: " . ($a >= $c ? "Verdadero" : "Falso") . "<br/>";
+            print"a <= c: " . ($a <= $c ? "Verdadero" : "Falso") . "<br/>";
+        ?>
+    </body>
+</html>
