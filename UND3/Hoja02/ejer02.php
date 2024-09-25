873086d462788b50877c61c0ea035e0fd812cd81
𝐄𝐥𝐬𝐚 <115722474+s0y3lsa@users.noreply.github.com>
2024-09-25 19:28:16 +0200

    Add files via upload

---
+++ UND3/Hoja02/ejer02.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php 
+
+    function sumaNum($num1,$num2){
+         
+        return $num1+$num2;
+    }
+
+    $num1=2;
+    $num2=2;
+    
+    print "El resultado de la suma es ". sumaNum($num1,$num2);
+
+?>
+
+</body>
+</html>
