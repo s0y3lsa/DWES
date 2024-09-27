b5e3f087ffb4721d737ac6009958e9394f5fb0b1
𝐄𝐥𝐬𝐚 <115722474+s0y3lsa@users.noreply.github.com>
2024-09-27 17:46:54 +0200

    Create Ejer11.php

---
+++ UND3/Hoja01/Ejer11.php
@@ -0,0 +1,10 @@
+<?php
+// Empezamos el primer bucle desde 10 hasta 1
+for ($i = 10; $i >= 1; $i--) {
+    // Imprimimos los valores decrecientes desde $i hasta 1
+    for ($j = $i; $j >= 1; $j--) {
+        echo $j . " ";
+    }
+    // Salto de línea después de cada fila
+    echo "\n";
+  ?>
