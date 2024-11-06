3aa1a879b638b424481ee94bf1b2592bcd09e415
unknown <elsa.ferreiracalvo@iesmiguelherrero.com>
2024-11-06 19:48:14 +0100

    lol

--- UND4/Hoja01_BBDD/public/index.php
+++ UND4/Hoja01_BBDD/public/index.php
@@ -1,7 +1,7 @@
 <?php
 require_once '../vendor/autoload.php';
 use App\clases\conexionBD;
-
+use App\clases\funcionesBD;
 $connection = conexionBD::getConnection();
 
 if ($connection instanceof PDO)
@@ -9,3 +9,36 @@ if ($connection instanceof PDO)
     echo 'Conexión establecida correctamente';
 }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+ 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+ 
+<body>
+    <h1>Jugadores de la NBA</h1>
+    <hr>
+ 
+    <form method="post">
+        <p>Equipos:
+            <select>
+                <?php
+                
+                
+
+                $registro = funcionesBD::getEquipos();
+              
+                foreach ($registro as $r) {
+                    echo '<option>' . $r . '</option>';
+                }
+                ?>
+            </select>
+        </p>
+        <input type="submit" value="Mostrar" name="Mostrar">
+    </form>
+</body>
+ 
+</html>
--- UND4/Hoja01_BBDD/src/clases/funcionesBD.php
+++ UND4/Hoja01_BBDD/src/clases/funcionesBD.php
@@ -1,3 +1,25 @@
 <?php
+namespace App\clases;
 
+use PDO;
+use PDOExceptionm;
+final class funcionesBD
+{
+    public static function getEquipos(): array
+    {
+        // Instancia de la conexión (asumiendo que `conexionBD` y su método `getConnection()` están definidos)
+        $dwes = conexionBD::getConnection();
+        // Consulta a la base de datos
+        $consulta = $dwes->query("SELECT nombre FROM equipos");
+        $resultado = [];
+        // Recorrer resultados
+        //array asociativo
+        while ($registro = $consulta->fetch(PDO::FETCH_ASSOC)) {
+            $resultado[] = $registro['nombre'];
+           
+        }
+        
+        return $resultado;
+    }
+}
 ?>
